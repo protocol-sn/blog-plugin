0bc32842ee0ea688ba7039effc7fc10123442ae8
John <patrick.meyerin@gmail.com>
2025-09-23 17:37:24 -0500

    undo unneeded changes

--- blog-plugin-ui/src/app/app.config.ts
+++ blog-plugin-ui/src/app/app.config.ts
@@ -4,10 +4,13 @@ import {provideHttpClient} from '@angular/common/http';
 import {provideOAuthClient} from 'angular-oauth2-oidc';
 import {InitializerService} from './initializer.service';
 import {provideMarkdown} from 'ngx-markdown';
+import {routes} from './app.routes';
+import {provideRouter} from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
     provideOAuthClient(),
     provideHttpClient(),
     provideAppInitializer(() => {
