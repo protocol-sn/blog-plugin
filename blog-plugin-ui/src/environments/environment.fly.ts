8ff101dd5e1094e3076dddbcd35f2ca4edc627a9
John <patrick.meyerin@gmail.com>
2025-09-24 16:30:21 -0500

    add back fly conf in ui

--- blog-plugin-ui/src/environments/environment.fly.ts
+++ blog-plugin-ui/src/environments/environment.fly.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  PRODUCTION: true,
   SERVICE_HOME: "https://psn-reverse-proxy.fly.dev/blog",
   OAUTH_ISSUER: "https://psn-reverse-proxy.fly.dev/auth/realms/social-network-ecosystem",
   OAUTH_CLIENT_ID: "blog-plugin-ui",
