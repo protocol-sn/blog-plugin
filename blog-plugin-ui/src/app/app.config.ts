575b56dce260d875e0ec5f920e1760b7dad2a1fc
John <patrick.meyerin@gmail.com>
2025-09-29 20:28:31 -0500

    hash routing breaks login

--- blog-plugin-ui/src/app/app.config.ts
+++ blog-plugin-ui/src/app/app.config.ts
@@ -5,12 +5,12 @@ import {provideOAuthClient} from 'angular-oauth2-oidc';
 import {InitializerService} from './initializer.service';
 import {provideMarkdown} from 'ngx-markdown';
 import {routes} from './app.routes';
-import {provideRouter, withHashLocation} from '@angular/router';
+import {provideRouter} from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withHashLocation()),
+    provideRouter(routes),
     provideOAuthClient(),
     provideHttpClient(),
     provideAppInitializer(() => {
